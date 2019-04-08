f03532790c206e38fcf1b7c953c0e4892fa7d994
arthur <1140735765@qq.com>
2019-04-08 17:32:47 +0800

    判断设备类型

---
+++ device.js
@@ -0,0 +1,69 @@
+const Device = (function (){
+    const userAgent = window.navigator.userAgent.toLowerCase()
+    let device = {};
+    let find = function(needle) {
+        return userAgent.indexOf(needle) !== -1
+    }
+    
+    device.iphone = function() {
+        return !device.windows() && find('iphone')
+    }
+    
+    device.ipod = function() {
+        return find('ipod')
+    }
+    
+    device.ipad = function() {
+        return find('ipad')
+    }
+    
+    device.android = function() {
+        return !device.windows() && find('android')
+    }
+    
+    device.androidPhone = function() {
+        return device.android() && find('mobile')
+    }
+    
+    device.blackberry = function() {
+        return find('blackberry') || find('bb10') || find('rim')
+    }
+    
+    device.blackberryPhone = function() {
+        return device.blackberry() && !find('tablet')
+    }
+    
+    device.windows = function() {
+        return find('windows')
+    }
+    
+    device.windowsPhone = function() {
+        return device.windows() && find('phone')
+    }
+    
+    device.fxos = function() {
+        return (find('(mobile') || find('(tablet')) && find(' rv:')
+    }
+    
+    device.fxosPhone = function() {
+        return device.fxos() && find('mobile')
+    }
+    
+    device.meego = function() {
+        return find('meego')
+    }
+    
+    device.mobile = function() {
+        return (
+            device.androidPhone() ||
+            device.iphone() ||
+            device.ipod() ||
+            device.windowsPhone() ||
+            device.blackberryPhone() ||
+            device.fxosPhone() ||
+            device.meego()
+        )
+    }
+    return device;
+})()
+export default Device
